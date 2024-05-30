27b2c3afd65d66f90e16031f33ddab7061d6dfc3
Adolf <fable.chernakov@mail.ru>
2024-05-30 19:22:07 +0500

    replacement modal-dropdown by select-dropdown

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,21 +10,21 @@ const StyledTextInput = styled(TextInput)
 interface InputProps {
     label: string;
     value: string;
-    action: any;
+    action: (value: string) => void;
   }
   
   export function Input({ label, value, action }: InputProps) {
     return (
-      <StyledView className={'flex h-max flex-row p-1 border-2 border-gray-200 bg-gray-700'}>
+      <StyledView className='flex h-max flex-row p-1 border-2 border-gray-200 bg-gray-700'>
   
-        <StyledView className={'flex w-24 justify-center items-center bg-gray-600'}>
-          <StyledText className={'text-1xl text-white'}>{label}</StyledText>
+        <StyledView className='flex w-24 justify-center items-center bg-gray-600'>
+          <StyledText className='text-1xl text-white'>{label}</StyledText>
         </StyledView>
   
-        <StyledView className={'w-1 h-full bg-gray-200'}></StyledView>
+        <StyledView className='w-1 h-full bg-gray-200'></StyledView>
   
-        <StyledView className='flex-1 m-2 flex-col justify-center'>
-          <StyledTextInput className={'text-1xl p-1 text-white bg-gray-600'} onChangeText={action} value={value} />
+        <StyledView className='flex-1 m-1'>
+          <StyledTextInput className='text-1xl p-1 text-white bg-gray-600' onChangeText={(value) => action(value)} value={value} />
         </StyledView>
   
       </StyledView>
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 // import { DayOfWeekName } from '../Lesson'
 
-import ModalDropdown from "react-native-modal-dropdown";
+import SelectDropdown from 'react-native-select-dropdown';
 import { styled } from 'nativewind';
 const StyledView = styled(View)
 const StyledText = styled(Text)
@@ -15,75 +15,45 @@ interface SelectProps {
 }
 
 export function Select({ label, options, defaultIndex = 0, action }: SelectProps) {
-  let dropdown: any;
   return (
     <StyledView className='flex h-max flex-row p-1 border-2 border-gray-200 bg-gray-700'>
       <StyledView className='flex w-24 justify-center items-center bg-gray-600'>
         <StyledText className='text-1xl text-white'>{label}</StyledText>
       </StyledView>
+
       <StyledView className='w-1 h-full bg-gray-200'></StyledView>
 
-      <StyledView className='flex-1 m-2 flex-col justify-center'>
-        <ModalDropdown
-          ref={el => dropdown = el}
-          defaultValue={options[defaultIndex]}
-          defaultIndex={defaultIndex}
-          style={styles.dropdown}
-          textStyle={styles.dropdown_text}
-          dropdownStyle={styles.dropdown_dropdown}
-          options={options}
-          renderButtonText={_renderButtonText}
-          renderRow={_renderRow}
-          renderSeparator={(selectionID, rowID, adjacentRowHighlighted) => _renderSeparator(selectionID, rowID, adjacentRowHighlighted)}
-          isFullWidth={true}
-          onSelect={(rowID) => action(+rowID)}
-          // onDropdownWillShow={ () => dropdown.show() }
-          // onDropdownWillHide={ () => dropdown.hide() }
+      <StyledView className='flex-1 m-1'>
+        <SelectDropdown
+          renderButton={(selectedItem, isOpened) => _renderButton(selectedItem, isOpened)}
+          renderItem={(item, index, isSelected) => _renderItem(item, index, isSelected)}
+          data={options}
+          defaultValueByIndex={defaultIndex}
+          onSelect={(selectedItem, index) => { action(index) }}
         />
       </StyledView>
     </StyledView>
   );
 
-  function _renderButtonText(text: any): string {
+  function _renderButton(selectedItem: any, isOpened: boolean): React.ReactNode {
     return (
-      text
-    );
-  }
-
-  function _renderRow(option: string, index: string, isSelected: boolean): ReactNode {
-    return (
-      <StyledView className='flex-1 p-2 justify-center items-center bg-gray-600'>
-        <StyledText className='text-1xl text-white'>{option}</StyledText>
+      <StyledView>
+        <StyledView className='p-1 justify-center items-center bg-gray-600'>
+          <StyledText className='text-1xl text-white'>
+            {selectedItem}
+          </StyledText>
+        </StyledView>
       </StyledView>
     );
   }
 
-  function _renderSeparator(selectionID: string, rowID: string, adjacentRowHighlighted: boolean): React.ReactNode {
-    if (+rowID == options.length - 1) return;
-    const key = `spr_${+rowID}`;
-    return (<StyledView className='h-px bg-gray-200'
-      key={key}
-    />);
+  function _renderItem(item: any, index: number, isSelected: boolean): React.ReactNode {
+    return (
+      <StyledView>
+        <StyledView className='flex-1 p-1 justify-center items-center bg-gray-600 border-2 border-gray-700'>
+          <StyledText className='text-1xl text-white'>{item}</StyledText>
+        </StyledView>
+      </StyledView>
+    );
   }
 }
-
-
-
-const styles = StyleSheet.create({
-  dropdown: {
-    textAlign: 'center',
-    backgroundColor: '#4b5563',
-  },
-  dropdown_text: {
-    flex: 1,
-    padding: 4,
-    fontSize: 14,
-    color: 'white',
-    textAlign: 'center',
-  },
-  dropdown_dropdown: {
-    minHeight: 10,
-    borderColor: '#e5e7eb',
-    padding: 1,
-  }, 
-});
